f1216b1d47e68e059fd411545fd550edbc9f55cf
Peter Wilmott <p@p8952.info>
2014-08-29 19:12:08 +0100

    If run as root, deploy dotfiles to /root not /home/root

--- modules/vim/manifests/init.pp
+++ modules/vim/manifests/init.pp
@@ -1,63 +1,69 @@
 class vim {
 
-	file { "/home/$::id/.vimrc":
+	if $::id == 'root' {
+		$home_dir = '/root'
+	} else {
+		$home_dir = "/home/$::id"
+	}
+
+	file { "$home_dir/.vimrc":
 		ensure => present,
 		source => 'puppet:///modules/vim/vimrc',
 	}
 
-	file { ["/home/$::id/.vim", "/home/$::id/.vim/autoload", "/home/$::id/.vim/bundle",]:
+	file { ["$home_dir/.vim", "$home_dir/.vim/autoload", "$home_dir/.vim/bundle",]:
 		ensure => directory,
-		before => File["/home/$::id/.vim/autoload/pathogen.vim"],
+		before => File["$home_dir/.vim/autoload/pathogen.vim"],
 	}
 
-	file { "/home/$::id/.vim/autoload/pathogen.vim":
+	file { "$home_dir/.vim/autoload/pathogen.vim":
 		ensure => present,
 		source => 'puppet:///modules/vim/pathogen.vim',
 	}
 
-	vcsrepo { "/home/$::id/.vim/bundle/ctrlp.vim":
+	vcsrepo { "$home_dir/.vim/bundle/ctrlp.vim":
 		ensure => present,
 		provider => git,
 		source => 'https://github.com/kien/ctrlp.vim',
 		revision => 'master',
 	}
 
-	vcsrepo { "/home/$::id/.vim/bundle/nerdtree":
+	vcsrepo { "$home_dir/.vim/bundle/nerdtree":
 		ensure => present,
 		provider => git,
 		source => 'https://github.com/scrooloose/nerdtree.git',
 		revision => 'master',
 	}
 
-	vcsrepo { "/home/$::id/.vim/bundle/supertab":
+	vcsrepo { "$home_dir/.vim/bundle/supertab":
 		ensure => present,
 		provider => git,
 		source => 'https://github.com/ervandew/supertab.git',
 		revision => 'master',
 	}
 
-	vcsrepo { "/home/$::id/.vim/bundle/tagbar":
+	vcsrepo { "$home_dir/.vim/bundle/tagbar":
 		ensure => present,
 		provider => git,
 		source => 'https://github.com/majutsushi/tagbar.git',
 		revision => 'master',
 	}
 
-	vcsrepo { "/home/$::id/.vim/bundle/vim-airline":
+	vcsrepo { "$home_dir/.vim/bundle/vim-airline":
 		ensure => present,
 		provider => git,
 		source => 'https://github.com/bling/vim-airline.git',
 		revision => 'master',
 	}
 
-	vcsrepo { "/home/$::id/.vim/bundle/vim-colors-wombat":
+	vcsrepo { "$home_dir/.vim/bundle/vim-colors-wombat":
 		ensure => present,
 		provider => git,
 		source => 'https://github.com/p8952/vim-colors-wombat.git',
 		revision => 'master',
 	}
 
-	vcsrepo { "/home/$::id/.vim/bundle/vim-gitgutter":
+	vcsrepo { "$home_dir/.vim/bundle/vim-gitgutter":
 		ensure => present,
 		provider => git,
 		source => 'https://github.com/airblade/vim-gitgutter.git',
