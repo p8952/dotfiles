179795bf1ba9b00b7cb769903612fa0deb217acc
Peter Wilmott <peter.wilmott@netdev.co.uk>
2015-09-04 11:52:11 +0100

    Replace Pathogen with Vundle

--- modules/vim/manifests/init.pp
+++ modules/vim/manifests/init.pp
@@ -6,102 +6,26 @@ class vim {
 
   file { "${home_dir}/.nvimrc":
     ensure => link,
-    target => "${home_dir}/.vimrc"
+    target => "${home_dir}/.vimrc",
   }
 
   file { [
     "${home_dir}/.vim",
-    "${home_dir}/.vim/autoload",
-    "${home_dir}/.vim/bundle"
+    "${home_dir}/.vim/bundle",
   ]:
     ensure => directory,
-    before => File["${home_dir}/.vim/autoload/pathogen.vim"],
+    before => Vcsrepo["${home_dir}/.vim/bundle/Vundle.vim"],
   }
 
   file { "${home_dir}/.nvim":
     ensure => link,
-    target => "${home_dir}/.vim"
+    target => "${home_dir}/.vim",
   }
 
-  file { "${home_dir}/.vim/autoload/pathogen.vim":
-    ensure => present,
-    source => 'puppet:///modules/vim/pathogen.vim',
-  }
-
-  vcsrepo { "${home_dir}/.vim/bundle/ctrlp.vim":
+  vcsrepo { "${home_dir}/.vim/bundle/Vundle.vim":
     ensure   => present,
     provider => git,
-    source   => 'https://github.com/kien/ctrlp.vim',
-    revision => 'master',
-  }
-
-  vcsrepo { "${home_dir}/.vim/bundle/nerdtree":
-    ensure   => present,
-    provider => git,
-    source   => 'https://github.com/scrooloose/nerdtree.git',
-    revision => 'master',
-  }
-
-  vcsrepo { "${home_dir}/.vim/bundle/syntastic":
-    ensure   => present,
-    provider => git,
-    source   => 'https://github.com/scrooloose/syntastic.git',
-    revision => 'master',
-  }
-
-  vcsrepo { "${home_dir}/.vim/bundle/tagbar":
-    ensure   => present,
-    provider => git,
-    source   => 'https://github.com/majutsushi/tagbar.git',
-    revision => 'master',
-  }
-
-  vcsrepo { "${home_dir}/.vim/bundle/vim-airline":
-    ensure   => present,
-    provider => git,
-    source   => 'https://github.com/bling/vim-airline.git',
-    revision => 'master',
-  }
-
-  vcsrepo { "${home_dir}/.vim/bundle/vim-colors-wombat":
-    ensure   => present,
-    provider => git,
-    source   => 'https://github.com/p8952/vim-colors-wombat.git',
-    revision => 'master',
-  }
-
-  vcsrepo { "${home_dir}/.vim/bundle/vim-elixir":
-    ensure   => present,
-    provider => git,
-    source   => 'https://github.com/elixir-lang/vim-elixir.git',
-    revision => 'master',
-  }
-
-  vcsrepo { "${home_dir}/.vim/bundle/vim-expand-region":
-    ensure   => present,
-    provider => git,
-    source   => 'https://github.com/terryma/vim-expand-region.git',
-    revision => 'master',
-  }
-
-  vcsrepo { "${home_dir}/.vim/bundle/vim-gitgutter":
-    ensure   => present,
-    provider => git,
-    source   => 'https://github.com/airblade/vim-gitgutter.git',
-    revision => 'master',
-  }
-
-  vcsrepo { "${home_dir}/.vim/bundle/vim-puppet":
-    ensure   => present,
-    provider => git,
-    source   => 'https://github.com/rodjek/vim-puppet.git',
-    revision => 'master',
-  }
-
-  vcsrepo { "${home_dir}/.vim/bundle/YouCompleteMe":
-    ensure   => present,
-    provider => git,
-    source   => 'https://github.com/Valloric/YouCompleteMe.git',
+    source   => 'https://github.com/VundleVim/Vundle.vim.git',
     revision => 'master',
   }
 }
