b47a5b81db9d08cd89103f7602ab9f20ac8e90a9
Peter Wilmott <p@p8952.info>
2015-10-16 13:22:14 +0100

    Populate world file with packages from hiera; Pull make.conf values from hiera

--- modules/portage/manifests/init.pp
+++ modules/portage/manifests/init.pp
@@ -5,6 +5,28 @@ class portage {
     mode  => 0644
   }
 
+  $cpu_flags          = hiera('cpu_flags')
+  $enabled_use_flags  = hiera('enabled_use_flags')
+  $disabled_use_flags = hiera('disabled_use_flags')
+  $video_cards        = hiera('video_cards')
+  $input_devices      = hiera('input_devices')
+  $packages           = hiera_array('packages')
+
+  define world_file() {
+    file_line { "world_file_${name}":
+      path   => '/var/lib/portage/world',
+      line   => $name,
+      notify => Exec['sort_world_file'],
+    }
+  }
+
+  world_file { $packages: }
+
+  exec { 'sort_world_file':
+    command     => '/usr/bin/sort /var/lib/portage/world -o /var/lib/portage/world',
+    refreshonly => true,
+  }
+
   file { '/etc/portage/make.conf':
     ensure  => present,
     content => template('portage/make.conf.erb')
