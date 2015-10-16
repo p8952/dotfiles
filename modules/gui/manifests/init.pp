6c0fac31247ba2fbae3fa51953861397e969ebbf
Peter Wilmott <p@p8952.info>
2015-10-16 14:47:52 +0100

    Make the gtk class a general gui class

--- modules/gui/manifests/init.pp
+++ modules/gui/manifests/init.pp
@@ -1,4 +1,4 @@
-class gtk {
+class gui {
   file { "${home_dir}/.config":
     ensure => directory,
   }
@@ -10,17 +10,31 @@ class gtk {
 
   file { "${home_dir}/.config/gtk-3.0/settings.ini":
     ensure  => present,
-    source  => 'puppet:///modules/gtk/gtk-3.0',
+    source  => 'puppet:///modules/gui/gtk-3.0',
     require => File["${home_dir}/.config/gtk-3.0"],
   }
 
   file { "${home_dir}/.gtkrc-2.0":
     ensure => present,
-    source => 'puppet:///modules/gtk/gtk-2.0',
+    source => 'puppet:///modules/gui/gtk-2.0',
   }
 
   exec { 'reload_gtk_theme':
     command     => '/usr/bin/python2 -c "import gtk; events=gtk.gdk.Event(gtk.gdk.CLIENT_EVENT); data=gtk.gdk.atom_intern(\"_GTK_READ_RCFILES\", False); events.data_format=8; events.send_event=True; events.message_type=data; events.send_clientmessage_toall();"',
     refreshonly => true,
+  }
+
+  $interface_ethernet = hiera('interface_ethernet')
+  $interface_wireless = hiera('interface_wireless')
+
+  file { "${home_dir}/.config/i3status":
+    ensure  => directory,
+    require => File["${home_dir}/.config"],
+  }
+
+  file { "${home_dir}/.config/i3status/config":
+    ensure  => present,
+    content => template('gui/i3status.erb'),
+    require => File["${home_dir}/.config/i3status"],
   }
 }
